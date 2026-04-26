97e1d2a7866c4350081b7320d09f5baf9ca09b87
Mohammad Usman Irshad <usmanirshad2006@gmail.com>
2026-04-26 12:05:42 +0500

    Extend Page_frame header with OPT lookahead support and dirty-bit tracking

--- Page_frame.h
+++ Page_frame.h
@@ -14,14 +14,16 @@ class Page_table {
 private:
 	unordered_map<unsigned long long, Page_frame_node> table;
 	list<unsigned long long >FIFO_queue;
-	int frame_count;
-	int max_frames;
+	
 	list<unsigned long long> LRU_queue;
 	unordered_map<unsigned long long, list<unsigned long long>::iterator> LRU_map;
 	Ram_memory mind_palace;
 	vector<unsigned long long> lookahead_buffer;
 
 public:
+	int frame_count;
+	int max_frames;
+	int lastEvictedFrame = -1;
 	unsigned long long dirty_evictions;
 	unsigned long long ram_read_count;
 	unsigned long long ram_write_count;
@@ -34,7 +36,15 @@ public:
 	void display();
 	void displayRam();
 	void LOOKAHEADINSERTION(FileConfiguration& config);
-
+	bool isFrameDirty(int frameIndex) {
+		
+		for (auto const& [vpn, node] : table) {
+			if (node.physical_memory / 4096 == frameIndex) {
+				return node.dirty_bit;
+			}
+		}
+		return false;
+	}
 	unsigned long long OPT_algorthim_for_getting_pshycal_number(unsigned long long vpn, unsigned long long offset, bool for_writing, TLB& l1,vector<unsigned long long> &raw_adresses, size_t index, char w = 'W');
 	unsigned long long accessPageTableDirectly(unsigned long long vpn, unsigned long long offset, bool for_writing, char w);
 	unsigned long long acccess_using_FIFO(unsigned long long vpn, unsigned long long offset, bool for_writing, TLB& l1, char w = 'W');
