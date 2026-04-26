66848113f5fa34f8b3a68c090fd631725d09bd4d
Mohammad Usman Irshad <usmanirshad2006@gmail.com>
2026-04-26 12:06:40 +0500

    Update main entry point to include raylib GUI window and visualization loop

--- OS_project.cpp
+++ OS_project.cpp
@@ -2,19 +2,27 @@
 #include "Virtual_Memory_manager.h"
 #include <fstream>
 #include <iomanip>
-#include <raylib.h>
+#include"Drawer.h"
 using namespace std;
 
 
 
 int main() {
      
-	Virtual_Memory_manager Sherlock; 
+	//Virtual_Memory_manager Sherlock; 
+	//cout << "this is the one with FIFO\n";
+	//Sherlock.Walker();
+	//cout << "This is the one with LRU\n";
+	//Sherlock.Runner();
+	//cout << "This is the one with OPT\n";
+	//Sherlock.SPRINTER();
+
+	Drawer Sherlock1;
 	cout << "this is the one with FIFO\n";
-	Sherlock.Walker();
+	Sherlock1.Walker1();
 	cout << "This is the one with LRU\n";
-	Sherlock.Runner();
+	Sherlock1.Runner1();
 	cout << "This is the one with OPT\n";
-	Sherlock.SPRINTER();
+	Sherlock1.SPRINTER1();
 
 }
