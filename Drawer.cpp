5f9ab663f7b3e7c6725917520a46aa6473fe20ea
Mohammad Usman Irshad <usmanirshad2006@gmail.com>
2026-04-26 12:03:52 +0500

    Add Drawer module for raylib-based GUI rendering of memory simulation state

---
+++ Drawer.cpp
@@ -0,0 +1,177 @@
+#include "Drawer.h"
+#include "mem_visualization.h"
+
+using namespace std;
+
+// 1. Standard simulation using LRU
+void Drawer::Runner1() {
+    ofstream clear_disk("HardDisk.txt", ios::trunc);
+    FileConfiguration config;
+    config.FileParser();
+    config.Reading_Addresses();
+
+    TLB cache(config);
+    Page_table pages(config);
+    MemoryVisualizer gui(1200, 800);
+
+    unsigned long long frame_address;
+    int instruction_count = 0;
+    auto it = config.actual_adress.begin();
+    bool hitStatus = false;
+
+    while (gui.IsRunning()) {
+        int activeIndex = -1;
+
+        if (it != config.actual_adress.end()) {
+            // CRITICAL: Reset trackers at the very start of processing
+            cache.lastEvictedSlot = -1;
+            pages.lastEvictedFrame = -1;
+
+            unsigned long long address = *it;
+            unsigned long long vpn = config.getVirtual_page_number(address);
+            unsigned long long offset = config.offset(address);
+
+            bool is_write = (instruction_count % 5 == 0);
+            char x = is_write ? 'W' : 'R';
+
+            hitStatus = cache.find(vpn, frame_address);
+            if (hitStatus) {
+                pages.getPhysicalAddress(vpn, offset, is_write, cache, hitStatus, x);
+            }
+            else {
+                // If miss, get physical frame from page table (may trigger RAM eviction)
+                unsigned long long p_frame = pages.getPhysicalAddress(vpn, offset, is_write, cache, x);
+                // Insert into TLB (may trigger TLB eviction)
+                cache.insert_with_LRU_algo(vpn, p_frame, true);
+            }
+
+            // Capture the index AFTER all potential movements/evictions
+            activeIndex = cache.getVPNIndex(vpn);
+
+            it++;
+            instruction_count++;
+
+            if (config.latency > 0) {
+                WaitTime((float)config.latency / 1000.0f);
+            }
+        }
+
+        gui.Render(cache, pages, hitStatus, config, activeIndex, "LRU Replacement");
+    }
+    cache.perfomance_metrics(config.latency, pages.ram_read_count, pages.ram_write_count, pages.dirty_evictions);
+}
+
+// 2. Optimal (OPT) Algorithm simulation
+void Drawer::SPRINTER1() {
+    ofstream clear_disk("HardDisk.txt", ios::trunc);
+    ofstream clear_disk2("OPT_logging", ios::trunc);
+    FileConfiguration config;
+    config.FileParser();
+    config.Reading_Addresses();
+
+    TLB cache(config);
+    Page_table pages(config);
+    pages.LOOKAHEADINSERTION(config);
+    MemoryVisualizer gui(1200, 800);
+
+    unsigned long long frame_address;
+    int instruction_count = 0;
+    size_t index = 0;
+    auto it = config.actual_adress.begin();
+    bool hitStatus = false;
+
+    while (gui.IsRunning()) {
+        int activeIndex = -1;
+
+        if (it != config.actual_adress.end()) {
+            cache.lastEvictedSlot = -1;
+            pages.lastEvictedFrame = -1;
+
+            unsigned long long address = *it;
+            unsigned long long vpn = config.getVirtual_page_number(address);
+            unsigned long long offset = config.offset(address);
+
+            bool is_write = (instruction_count % 5 == 0);
+            char x = is_write ? 'W' : 'R';
+
+            hitStatus = cache.find(vpn, frame_address);
+            if (hitStatus) {
+                pages.accessPageTableDirectly(vpn, offset, is_write, x);
+            }
+            else {
+                unsigned long long p_frame = pages.accessPageTableDirectly(vpn, offset, is_write, x);
+                if (p_frame == (unsigned long long) - 1) {
+                    p_frame = pages.OPT_algorthim_for_getting_pshycal_number(vpn, offset, is_write, cache, config.actual_adress, index, x);
+                }
+                cache.insert_with_LRU_algo(vpn, p_frame, true);
+            }
+
+            activeIndex = cache.getVPNIndex(vpn);
+            it++;
+            index++;
+            instruction_count++;
+
+            if (config.latency > 0) {
+                WaitTime((float)config.latency / 1000.0f);
+            }
+        }
+        gui.Render(cache, pages, hitStatus, config, activeIndex, "Optimal Replacement");
+    }
+    cache.perfomance_metrics(config.latency, pages.ram_read_count, pages.ram_write_count, pages.dirty_evictions);
+}
+
+// 3. FIFO Algorithm simulation
+void Drawer::Walker1() {
+    ofstream clear_disk("HardDisk.txt", ios::trunc);
+    FileConfiguration config;
+    config.FileParser();
+    config.Reading_Addresses();
+
+    TLB cache(config);
+    Page_table pages(config);
+    MemoryVisualizer gui(1200, 800);
+
+    unsigned long long frame_address;
+    int instruction_count = 0;
+    auto it = config.actual_adress.begin();
+    bool hitStatus = false;
+
+    while (gui.IsRunning()) {
+        int activeIndex = -1;
+
+        if (it != config.actual_adress.end()) {
+            cache.lastEvictedSlot = -1;
+            pages.lastEvictedFrame = -1;
+
+            unsigned long long address = *it;
+            unsigned long long vpn = config.getVirtual_page_number(address);
+            unsigned long long offset = config.offset(address);
+
+            bool is_write = (instruction_count % 5 == 0);
+            char x = is_write ? 'W' : 'R';
+
+            hitStatus = cache.findFIFOTLB(vpn, frame_address);
+            if (hitStatus) {
+                pages.accessPageTableDirectly(vpn, offset, is_write, x);
+            }
+            else {
+                unsigned long long p_frame = pages.accessPageTableDirectly(vpn, offset, is_write, x);
+                if (p_frame == (unsigned long long) - 1) {
+                    p_frame = pages.acccess_using_FIFO(vpn, offset, is_write, cache, x);
+                }
+                cache.Insert_with_FIFO_algo(vpn, p_frame, true);
+            }
+
+            activeIndex = cache.getVPNIndex(vpn);
+            it++;
+            instruction_count++;
+
+            if (config.latency > 0) {
+                WaitTime((float)config.latency / 1000.0f);
+            }
+        }
+
+        gui.Render(cache, pages, hitStatus, config, activeIndex, "FIFO Replacement");
+    }
+    cache.perfomance_metrics(config.latency, pages.ram_read_count, pages.ram_write_count, pages.dirty_evictions);
+}
---
+++ Drawer.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <json/json.h>
+#include "FileConfiguration.h"
+#include "TLB_table.h"
+#include "Page_frame.h"// Important: Ensure this name matches your file
+
+using namespace std;
+
+class Drawer {
+public:
+    void Runner1();
+    void SPRINTER1();
+    void Walker1();
+};
